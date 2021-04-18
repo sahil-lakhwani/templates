08db843487a965ebc802a3d5a793cff59d69507b
sahil-lakhwani <sahilakhwani@gmail.com>
2021-04-18 19:23:58 +0530

    initial commit
    Signed-off-by: sahil-lakhwani <sahilakhwani@gmail.com>

---
+++ go.mod
@@ -0,0 +1,21 @@
+module try
+
+go 1.16
+
+require (
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/Masterminds/sprig v2.22.0+incompatible
+	github.com/dgrijalva/jwt-go v0.0.0-20160705203006-01aeca54ebda // indirect
+	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/gophercloud/gophercloud v0.0.0-20190126172459-c818fa66e4c8 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/mitchellh/copystructure v1.1.2 // indirect
+	github.com/pkg/errors v0.9.1
+	golang.org/x/crypto v0.0.0-20210415154028-4f45737414dc // indirect
+	k8s.io/apimachinery v0.21.0
+	k8s.io/client-go v0.21.0
+	k8s.io/klog v0.3.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"path/filepath"
+
+	"text/template"
+	"github.com/Masterminds/sprig"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+
+	// variable values to be substituted
+	values := map[string]interface{}{
+		"replicaCount": 3,	
+	}
+
+
+	// example K8s manifest
+	// checks if a storage class "gp2" is available, else uses "standard"
+	manifest := `apiVersion: zookeeper.pravega.io/v1beta1
+        kind: ZookeeperCluster
+        metadata:
+            name: zookeeper
+            namespace: zookeeper
+        spec:
+        	{{- $storageClass := "gp2" -}}
+            replicas: {{ .replicaCount}}
+            storageClass: {{ if (lookup "storage.k8s.io/v1" "StorageClass" "" $storageClass) }}"gp2"{{ else }}"standard"{{ end }}
+            `
+
+
+    // fmap can be extended with any custom functions.
+	fmap := funcMap()
+    t := template.Must(template.New("test").Funcs(fmap).Parse(manifest))
+
+	err := t.Execute(os.Stdout, values)
+	if err != nil {
+		fmt.Printf("Error during template execution: %s", err)
+		return
+	}
+}
+
+func funcMap() template.FuncMap {
+	fmap := sprig.TxtFuncMap()
+
+	config := kubeConfig()
+
+	fmap["lookup"] = NewLookupFunction(config)
+
+	return fmap
+}
+
+func kubeConfig() *rest.Config {
+	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	fmt.Println("Using kubeconfig file: ", kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return config
+}
